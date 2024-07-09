526b2f3a62573992ccee50eeea6e40976e112833
mt-sette <matteo.sette@digitalcareerinstitute.org>
2024-07-09 13:29:40 +0200

    Add style to Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,19 @@
 import { useContext } from 'react';
 import { ThemeContext } from '../context/ThemeContext.jsx';
+
+const style = {
+    padding: '0.5rem 1rem',
+    borderRadius: '5%',
+    backgroundColor: 'tomato',
+};
+
 const Button = ({ children }) => {
     const { setTheme, theme } = useContext(ThemeContext);
     return (
-        <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+        <button
+            style={{ ...style }}
+            onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+        >
             {children}
         </button>
     );
